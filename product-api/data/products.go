146ff06053043e3236ab8229a63bb2f94e3f1e0b
Pan Shaun <Shaun.Pan@fil.com>
2021-09-05 20:14:59 +0800

    Adding CRUD for the api

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -21,12 +22,56 @@ type Product struct {
 // Products is a collection of product
 type Products []*Product
 
-func (p Products) ToJSON(w io.Writer) error {
+// ToJSON serializes the contents of the collection ([]*product) to JSON
+// NewEncoder provides better performance than json.Unmarshal as it does not
+// have to buffer the ouput into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
+
+func (p *Product) FromJSON(r io.Reader) error {
+	decoder := json.NewDecoder(r)
+	return decoder.Decode(p)
+}
+
+// GetProducts return a list of products
 func GetProducts() Products {
 	return productList
+}
+
+func AddProducts(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProducts(id int, p *Product) error {
+
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lastProduct := productList[len(productList)-1]
+	return lastProduct.ID + 1
 }
 
 var productList = []*Product{
--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
-	"github.com/shaun7pan/building-microservices-gin/product-api/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/shaun7pan/building-microservices-gin/product-api/data"
 )
 
 type Products struct {
@@ -15,9 +18,40 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 // ServeHTTP is the main entry point for the handler and satisfies the http.Handler interface
-func (p Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+
+		// expect the id in the URI
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+
+		idStr := g[0][1]
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProducts(id, rw, r)
 		return
 	}
 
@@ -27,8 +61,8 @@ func (p Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 }
 
-func (p Products) getProducts(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle GET Products")
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Product")
 
 	// fetch the products from datastore
 	ps := data.GetProducts()
@@ -36,4 +70,37 @@ func (p Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Printf("Handle POST product")
+
+	// fetch new product from request
+	newProd := &data.Product{}
+	err := newProd.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to marchal json", http.StatusBadRequest)
+	}
+	data.AddProducts(newProd)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handling PUT requests.")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to marchal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProducts(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+	}
+	return
 }
